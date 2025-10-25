8233c7441a5bd790211d866dfdc55aa2b9ae7d78
David Onoh <davidonoh@gmail.com>
2025-10-25 14:06:38 +0100

    Initial Commit

---
+++ Pose Detection Assesment/Pose_Detection_AssesmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pose_Detection_AssesmentApp.swift
+//  Pose Detection Assesment
+//
+//  Created by DavidOnoh on 10/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Pose_Detection_AssesmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
